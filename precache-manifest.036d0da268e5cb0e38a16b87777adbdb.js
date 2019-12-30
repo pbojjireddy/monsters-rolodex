95c849eb72e04c38fdb812802b53c705a20342c5
Pavan Reddy <ajay.bojji@gmail.com>
2019-12-30 18:06:32 +0530

    Updates

---
+++ precache-manifest.036d0da268e5cb0e38a16b87777adbdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00540840d33ff4722f3817529ec6015c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1391379c6486cc6c005b",
+    "url": "/monsters-rolodex/static/css/main.d3681866.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1391379c6486cc6c005b",
+    "url": "/monsters-rolodex/static/js/main.eee329bf.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
